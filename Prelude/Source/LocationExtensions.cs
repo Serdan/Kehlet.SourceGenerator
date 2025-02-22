bec5a0897e9afef1b4bd9488430a9eecda010eee
Anders Kehlet <ake@kruso.dk>
2025-02-22 12:03:01 +0100

    Cacheable location

---
+++ Prelude/Source/LocationExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Kehlet.SourceGenerator;
+
+public static class LocationExtensions
+{
+    /// <summary>
+    /// <see cref="Location"/>s may hold references to the <see cref="SyntaxTree"/>. This method creates a new <see cref="Location"/> with only cacheable data.
+    /// </summary>
+    /// <param name="location"></param>
+    /// <returns></returns>
+    public static Location ToCacheable(this Location location) =>
+        Location.Create(location.SourceTree?.FilePath ?? string.Empty, location.SourceSpan, location.GetLineSpan().Span);
+}
